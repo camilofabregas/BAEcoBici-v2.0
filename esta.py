05bfc5ea8e59b6e7cbeef599d62b9acfb6d12f53
marifortu45 <50252785+marifortu45@users.noreply.github.com>
2019-06-19 14:21:53 -0300

    Add files via upload

--- esta.py
+++ esta.py
@@ -8,13 +8,9 @@ def cargarEstaciones (): #estaciones seria el diccionario vacio al cual le voy a
 	lectorEstaciones = csv.reader(archivoEstaciones, skipinitialspace=True)
 	for estacion in lectorEstaciones:
 		for x in range(1,int(estacion[4])):		
-			estaciones[int(estacion[3])] = {"Dirección": estacion[2] , "Latitud y longitud": (float(estacion[1]), float(estacion[0])) , "Capacidad": int(estacion[4]) , "Bicicletas": [] }
+			estaciones[int(estacion[3])] = {"Dirección": estacion[2] , "Latitud y longitud": (float(estacion[1]), float(estacion[0])) , "Capacidad": int(estacion[4]) , "Bicicletas": {} }
 			for anclaje in range(1, estaciones[int(estacion[3])]["Capacidad"] + 1 ):
-				estaciones[int(estacion[3])]["Bicicletas"].append([anclaje, ""])
-
-		#longitud, latitud, direccion, identificador, capacidad
-		#identificador, direcciones, latitudLongitud, anclajesTotales, anclajesOcupados
-	
+				estaciones[int(estacion[3])]["Bicicletas"][anclaje] = ""
 	archivoEstaciones.close()
 	return estaciones
 
@@ -32,23 +28,39 @@ def cargarBicicletas ():
 
 def repartirBicicletasEstacion (estaciones, bicicletas):
 	lBicicletas = sorted(bicicletas.keys())
+	bicisTotalesAsignadas = 0
+	lBicisParaRepartir = []
 	for estacion in estaciones:
-		BicicletasRetiradas = random.randrange(7,estaciones[estacion]["Capacidad"])
-		anclaje = 0
-		for x in range(BicicletasRetiradas):
+		bicicletasRetiradas = random.randrange(7, estaciones[estacion]["Capacidad"] + 1)
+		bicisTotalesAsignadas += bicicletasRetiradas
+		lBicisParaRepartir += [bicicletasRetiradas]
+	while bicisTotalesAsignadas != len(lBicicletas):
+		lBicisParaRepartir.clear()
+		bicisTotalesAsignadas = 0
+		for estacion in estaciones:
+			bicicletasRetiradas = random.randrange(7, estaciones[estacion]["Capacidad"] + 1)
+			bicisTotalesAsignadas += bicicletasRetiradas
+			lBicisParaRepartir += [bicicletasRetiradas]
+	contador = 0
+	for estacion in estaciones:
+		anclaje = 1
+		for x in range(lBicisParaRepartir[contador]):
 			biciRetirada = random.choice(lBicicletas)
-			estaciones [estacion]["Bicicletas"][anclaje][1] = biciRetirada
+			estaciones [estacion]["Bicicletas"][anclaje] = biciRetirada
 			anclaje += 1
 			lBicicletas.remove(biciRetirada)
-
-	return estaciones
+		contador += 1
+	return estaciones, lBicicletas
 
 def leer(archivo, fin):
 	estacion = archivo.readline()
 	lista = estacion.rstrip().split(",")
 	return lista if estacion else fin
 
+"""
 estaciones, bicicletas = cargarEstaciones(), cargarBicicletas()
-estaciones = repartirBicicletasEstacion(estaciones, bicicletas)
+estaciones, bicis = repartirBicicletasEstacion(estaciones, bicicletas)
 
 print(estaciones)
+print(bicis) # esto es para que vean que queda vacia
+"""
