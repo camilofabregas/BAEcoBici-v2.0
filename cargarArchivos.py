6cf2cd76b4c5bdc9bd1fe54b9615236cc4a75ae6
Camilo Fábregas <50251894+MiloGNR@users.noreply.github.com>
2019-06-19 16:55:41 -0300

    Update cargarArchivos.py

--- cargarArchivos.py
+++ cargarArchivos.py
@@ -1,9 +1,13 @@
+import random
+import csv
+
 def cargarArchivos():
 	usuarios, bicicletas, estaciones, viajesEnCurso, viajesFinalizados = dict(), dict(), dict(), dict(), dict()
-	archBicicletas = open("bicicletas.csv", "r")
-	archEstaciones = open("estaciones.csv", "r")
-	archViajesFinalizados = open("viajes.csv", "r")
 	chequearUsuariosMaestro(usuarios)
+	cargarEstaciones(estaciones)
+	cargarBicicletas(bicicletas)
+	repartirBicicletasEstacion(estaciones, bicicletas)
+	cargar
 	#cargarArchivoEnMemoria(archBicicletas, bicicletas)
 	#cargarArchivoEnMemoria(archEstaciones, estaciones)
 	#cargarArchivoEnMemoria(archViajesFinalizados, viajesFinalizados)
@@ -60,6 +64,53 @@ def cargarUsuarios(usuarios, archUsuariosMaestro):
 	while int(usuario[2]) != fin[2]:
 		usuarios[int(usuario[2])] = [usuario[3], usuario[0].lower().replace(" ", "_"), usuario[1]]
 		usuario = leer(archUsuariosMaestro, fin)
+
+def cargarEstaciones(estaciones):
+	archivoEstaciones = open("estaciones.csv","r",encoding = "utf-8")
+	next(archivoEstaciones)
+	lectorEstaciones = csv.reader(archivoEstaciones, skipinitialspace=True)
+	for estacion in lectorEstaciones:
+		for x in range(1,int(estacion[4])):		
+			estaciones[int(estacion[3])] = {"Dirección": estacion[2] , "Latitud y longitud": (float(estacion[1]), float(estacion[0])) , "Capacidad": int(estacion[4]) , "Bicicletas": {} }
+			for anclaje in range(1, estaciones[int(estacion[3])]["Capacidad"] + 1 ):
+				estaciones[int(estacion[3])]["Bicicletas"][anclaje] = ""
+	archivoEstaciones.close()
+
+def cargarBicicletas(bicicletas):
+	fin = ["",999999999]
+	archivoBicicletas = open("bicicletas.csv","r", encoding = "utf-8")
+	next(archivoBicicletas)
+	bicicleta = leer(archivoBicicletas, fin)
+	while int(bicicleta[1]) != fin[1]:
+		bicicletas[int(bicicleta[1])] = ["En condiciones", "Anclada en estación"]
+		bicicleta = leer(archivoBicicletas, fin)
+	archivoBicicletas.close()
+
+def repartirBicicletasEstacion(estaciones, bicicletas):
+	lBicicletas = sorted(bicicletas.keys())
+	bicisTotalesAsignadas = 0
+	lBicisParaRepartir = []
+	for estacion in estaciones:
+		bicicletasRetiradas = random.randrange(7, estaciones[estacion]["Capacidad"] + 1)
+		bicisTotalesAsignadas += bicicletasRetiradas
+		lBicisParaRepartir += [bicicletasRetiradas]
+	while bicisTotalesAsignadas != len(lBicicletas):
+		lBicisParaRepartir.clear()
+		bicisTotalesAsignadas = 0
+		for estacion in estaciones:
+			bicicletasRetiradas = random.randrange(7, estaciones[estacion]["Capacidad"] + 1)
+			bicisTotalesAsignadas += bicicletasRetiradas
+			lBicisParaRepartir += [bicicletasRetiradas]
+	contador = 0
+	for estacion in estaciones:
+		anclaje = 1
+		for x in range(lBicisParaRepartir[contador]):
+			biciRetirada = random.choice(lBicicletas)
+			estaciones [estacion]["Bicicletas"][anclaje] = biciRetirada
+			anclaje += 1
+			lBicicletas.remove(biciRetirada)
+		contador += 1
+	return estaciones, lBicicletas
 
 def leer(archivo, fin):
 	linea = archivo.readline()
