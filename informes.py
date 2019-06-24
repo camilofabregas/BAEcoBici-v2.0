22f2cc8808e8a99e1aaaf9417c92a4f4e2fd76c8
marifortu45 <50252785+marifortu45@users.noreply.github.com>
2019-06-24 15:21:17 -0300

    Update informes.py

--- informes.py
+++ informes.py
@@ -14,45 +14,54 @@ def topUsuariosCantidadViajes(usuarios, viajesFinalizados):
 def topUsuariosDuracionViajes(viajesFinalizados):
 	usuariosMasDuracion = {}
 	for dni in viajesFinalizados:
+		duracionDias = 0
 		duracionHora = 0
 		duracionMin = 0
 		duracionSeg = 0
 		for viaje in range(len(viajesFinalizados[dni])):
 			salida = viajesFinalizados[dni][viaje][2]
 			llegada = viajesFinalizados[dni][viaje][4]
-			diferenciaHora = abs((llegada.hour - salida.hour))
-			diferenciaMinutos = abs((llegada.minute - salida.minute))
-			diferenciaSegundos = abs((llegada.second - salida.second))
+			acumuladorSalida = (salida.hour * 3600) + (salida.minute * 60) + salida.second
+			if salida <= llegada:
+				acumuladorLlegada = (llegada.hour * 3600) + (llegada.minute * 60) + llegada.second
+			else:
+				acumuladorLlegada = ((llegada.hour + 24) * 3600) + (llegada.minute * 60) + llegada.second
+			diferencia = acumuladorLlegada - acumuladorSalida
+			diferenciaSegundos = diferencia % 60
+			diferenciaMinutos = diferencia // 60
+			if diferenciaMinutos >= 60:
+				diferenciaHora = diferenciaMinutos // 60
+				diferenciaMinutos = diferenciaMinutos % 60
+			else:
+				diferenciaHora = 0
+			
 			duracionHora += diferenciaHora
 			duracionMin += diferenciaMinutos
 			duracionSeg += diferenciaSegundos
-			if duracionSeg >= 60:
-				duracionMin += 1
-				duracionSeg = duracionSeg - 60
-			if duracionMin >= 60:
-				duracionHora += 1
-				duracionMin = duracionMin - 60
-		duracion = time(duracionHora, duracionMin, duracionSeg)
-		if dni not in usuariosMasDuracion:
-			usuariosMasDuracion[dni] = duracion
-		else:
-			sumaSegundos = (usuariosMasDuracion[dni].second + duracion.second)
-			if sumaSegundos >= 60:
-				sumaMinutos = (usuariosMasDuracion[dni].minute + duracion.minute) + 1
-				sumaSegundos = sumaSegundos - 60
-			else:
-				sumaMinutos = (usuariosMasDuracion[dni].minute + duracion.minute)
-			if sumaMinutos >= 60:
-				sumaHoras = (usuariosMasDuracion[dni].hour + duracion.hour)
-				sumaMinutos = sumaMinutos - 60
-			else: 
-				sumaHoras = (usuariosMasDuracion[dni].hour + duracion.hour)
-			usuariosMasDuracion[dni] = time(sumaHoras, sumaMinutos, sumaSegundos)
+			
+		if duracionSeg >= 60:
+			masMinutos = duracionSeg // 60
+			segundos = diferenciaSegundos % 60
+			duracionMin += masMinutos
+			duracionSeg = segundos
+		if duracionMin >= 60:
+			masHoras = duracionMin // 60
+			minutos = duracionMin % 60
+			duracionHora += masHoras
+			duracionMin = minutos
+		if duracionHora >= 24:
+			masDias = duracionHora // 24
+			horas = duracionHora % 24
+			duracionDias += masDias
+			duracionHora = horas
+		duracion = [duracionDias, time(duracionHora, duracionMin, duracionSeg)]
+		usuariosMasDuracion[dni] = duracion
 	print("\n**** USUARIOS CON MAYOR TIEMPO DE VIAJE ****\n")
 	topOrdenado = sorted(usuariosMasDuracion.items(), key = lambda x: x[1], reverse = True)
 	contador = 1
 	for usuarios in topOrdenado[0:5]:
-		print(contador,"El usuario {} con {} hs de viaje.".format(usuarios[0], usuarios[1]))
+		print(contador,"El usuario {} con {} dias y {} hs de viaje.".format(usuarios[0], usuarios[1][0], usuarios[1][1]))
+		#print(contador,"El usuario {} con {} hs de viaje.".format(usuarios[0], usuarios[1]))
 		contador +=1
 
 def bicicletasEnReparacion (bicicletas):
